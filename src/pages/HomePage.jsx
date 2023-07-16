67563417e9c9ee3eaaf9386e54c3beb6e7b9de02
juba789 <jubasyfax87@gmail.com>
2023-07-16 23:55:28 +0200

    creation outlet pour route et reorganisations des composants

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./HomePage.scss"
+import Banner from '../layout/Banner' 
+import AppartGrid from '../components/AppartGrid'
+
+
+
+
+function HomePage() {
+  return (
+    <>
+    
+       <Banner/>
+       <AppartGrid/>
+    
+    </>
+  )
+}
+
+export default HomePage
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,45 +1,45 @@
 import React from 'react';
-import { createBrowserRouter } from "react-router-dom";
-import App from '../App';
+import { createBrowserRouter, Outlet } from "react-router-dom";
+import HomePage from '../pages/HomePage';
 import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import Footer from '../layout/Footer';
+import Main from '../layout/Main'
 
-const router = createBrowserRouter([
-    {
-      path: "/",
-      element: (
-        <>
-        <Navbar />
-        <App />
-        <Footer />
-        </>
-  
-      ),
-      errorElement:<h1>404 not found</h1>
-    },
-    {
-      path: "/flat",
-      element:(
-        <>
-        <Navbar />
-        <h1>Nos appartements</h1>
-        <Footer />
-     </>
-      )
-       },
-    {
-      path: "/about",
-      element:(
-        <>
-        <Navbar />
-        <h1>A propos</h1>
-        <Footer />
-        </>
-  )
-    }
-   
-  ]);
+const HeaderFooterLayout=()=>{
+  return(
+    <>
+    <Navbar />
+    <Main>
+    <Outlet/>
+    </Main>
+    <Footer/>
+   </>
 
-  export { router };
+  );
+}
+
+const router =createBrowserRouter ([
+  {
+    element:<HeaderFooterLayout/>,
+    errorElement:<h1>404 not found</h1>,
+    children:[
+      {
+        path:'/',
+        element:<HomePage/>
+      },
+      {
+       path:'/flat',
+       element:<h1>Appartement</h1>
+      },
+      {
+        path:'/about',
+        element:<h1>A propos</h1>
+
+      }
+    ]
+}
+])
+
+export { router };
 
   
