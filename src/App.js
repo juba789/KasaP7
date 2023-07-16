4f46437e178145b6530d65c891cf50a317fdba9f
juba789 <jubasyfax87@gmail.com>
2023-07-16 09:16:50 +0200

    configuration fichier en scss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./App.css"
+import "./App.scss"
 import Banner from './components/Banner' 
 import AppartGrid from './components/AppartGrid'
 import Main from './components/Main'
