62dafe3968013f0e3bc9968d8a10622f3f04dff5
juba789 <jubasyfax87@gmail.com>
2023-07-29 02:27:04 +0200

    correction responsivité navigation et page appartement

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 
 function Navbar() {
   return (
-   <nav className='navbar' >
+ /*  <nav className='navbar' >
     <NavLink to='/'>
       <div className='navbar__logo'>
         <img src='logo.png' alt='Logo'/>
@@ -15,6 +15,25 @@ function Navbar() {
         <div>A propos</div>
         </NavLink>
    </nav>
+ */ <nav className='navbar'>
+    <div class='navbar__logo'>
+      <NavLink to='/'>
+        <img src="LOGOKASA.png" />
+      </NavLink>
+    </div>
+    <div class="navig">
+      <div class="accueil">
+        <NavLink to='/'>
+          Accueil
+        </NavLink>
+      </div>
+      <div class="apropos">
+        <NavLink to='/about'>
+          A propos
+        </NavLink>
+      </div>
+    </div>
+  </nav>  
   )
 }
 
