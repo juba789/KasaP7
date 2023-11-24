2b3ee877aebcc54cd4889077980e247185c70123
juba789 <jubasyfax87@gmail.com>
2023-11-24 03:55:31 +0100

    arrangement

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,51 +1,45 @@
-import React from 'react';
+import React from "react";
 import { createBrowserRouter, Outlet } from "react-router-dom";
-import HomePage from '../pages/HomePage';
-import Navbar from '../layout/Navbar';
-import Footer from '../layout/Footer';
-import Main from '../layout/Main'
-import AppartPage from '../pages/AppartPage';
-import About from '../pages/About'
-import ErrorPage from '../pages/ErrorPage';
+import HomePage from "../pages/HomePage";
+import Navbar from "../layout/Navbar";
+import Footer from "../layout/Footer";
+import Main from "../layout/Main";
+import AppartPage from "../pages/AppartPage";
+import About from "../pages/About";
+import ErrorPage from "../pages/ErrorPage";
 
-const HeaderFooterLayout=()=>{
-  return(
+const HeaderFooterLayout = () => {
+  return (
     <>
-    <Navbar />             
-    <Main>
-    <Outlet/>
-    </Main>
-    <Footer/>
-   </>
-
+      <Navbar />
+      <Main>
+        <Outlet />
+      </Main>
+      <Footer />
+    </>
   );
-}
+};
 
-const router =createBrowserRouter ([
+const router = createBrowserRouter([
   {
-    element:<HeaderFooterLayout/>,
-   errorElement:<ErrorPage/>,
-    children:[
+    element: <HeaderFooterLayout />,
+    errorElement: <ErrorPage />,
+    children: [
       {
-        path:'/',
-        element:<HomePage/>
+        path: "/",
+        element: <HomePage />,
       },
       {
-        
-       path: '/flat/:appartId',
-       element:<AppartPage/>
+        path: "/flat/:appartId",
+        element: <AppartPage />,
       },
-      
-      {
-        path:'/about',
-        element:<About />
 
-      }
-     
-    
-    
-    ]
-}
-])
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
+  },
+]);
 
 export { router };
